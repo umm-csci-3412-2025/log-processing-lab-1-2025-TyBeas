3095ddf62bd93377e82152ab71e445076a9e867d
TyBeas <beasl110@morris.umn.edu>
2025-01-30 22:05:31 -0600

    changing create hours dist

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,13 +1,12 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 SCRATCH=$(mktemp -d)
 home=$(pwd)
 cd "$1" || exit
 cat "$home/html_components/hours_dist_header.html" > hours_dist.html
 for f in */; do
-    [[ -d $f ]] || break
+    [[ -d $f ]] || continue
     awk '{print $2}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
-
 done
-sort "$SCRATCH/listOfHours.txt" | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);" >> hours_dist.html}'
+sort "$SCRATCH/listOfHours.txt" | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' >> hours_dist.html
 cat "$home/html_components/hours_dist_footer.html" >> hours_dist.html
