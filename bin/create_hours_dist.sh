2cd8b21a8c268acd98a42a062f62c2d8d43bc9bd
TyBeas <beasl110@morris.umn.edu>
2025-02-03 12:34:38 -0600

    fixing create hours

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-
 SCRATCH=$(mktemp -d)
 home=$(pwd)
 cd "$1" || exit
 cat "$home/html_components/hours_dist_header.html" > hours_dist.html
 for f in */; do
     [[ -d $f ]] || continue
-    awk '{print $1}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
+    awk '{print $2}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
 done
 sort "$SCRATCH/listOfHours.txt" | uniq -c | sort -k2,2n | awk '{printf "data.addRow([\x27%02d\x27, %d]);\n", $2, $1}' >> hours_dist.html
+
 cat "$home/html_components/hours_dist_footer.html" >> hours_dist.html
+rm -r "$SCRATCH"
