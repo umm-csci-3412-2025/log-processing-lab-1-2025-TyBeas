185346252ee5b89da819e6423e1beb7522b772a6
TyBeas <beasl110@morris.umn.edu>
2025-02-03 12:15:31 -0600

    trying to fix sorting

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -8,5 +8,5 @@ for f in */; do
     [[ -d $f ]] || continue
     awk '{print $2}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
 done
-sort -n "$SCRATCH/listOfHours.txt" | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' >> hours_dist.html
+sort "$SCRATCH/listOfHours.txt" | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' >> hours_dist.html
 cat "$home/html_components/hours_dist_footer.html" >> hours_dist.html
