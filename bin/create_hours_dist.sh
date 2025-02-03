c63f554388f293ab952a12d5e206f43d236fa0cd
TyBeas <beasl110@morris.umn.edu>
2025-02-03 12:19:29 -0600

    Maybe this time

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -6,7 +6,7 @@ cd "$1" || exit
 cat "$home/html_components/hours_dist_header.html" > hours_dist.html
 for f in */; do
     [[ -d $f ]] || continue
-    awk '{print $2}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
+    awk '{print $1}' "$f/failed_login_data.txt" >> "$SCRATCH/listOfHours.txt"
 done
 sort "$SCRATCH/listOfHours.txt" | uniq -c | sort -k2,2n | awk '{printf "data.addRow([\x27%02d\x27, %d]);\n", $2, $1}' >> hours_dist.html
 cat "$home/html_components/hours_dist_footer.html" >> hours_dist.html
