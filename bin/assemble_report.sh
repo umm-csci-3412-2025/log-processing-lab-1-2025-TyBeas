d4f5801a9342b247e4ca6a3ba15adabcfb79919d
TyBeas <beasl110@morris.umn.edu>
2025-02-04 14:25:46 -0600

    Fixing last error

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -7,4 +7,4 @@ touch failed_login_summary.html
 
 cat ./country_dist.html ./hours_dist.html ./username_dist.html > content.html
 
-../bin/wrap_contents.sh ./content.html ../html_components/summary_plots ./failed_login_summary.html
+cat ./html_components/header.html ./content.html ./html_components/footer.html > failed_login_summary.html
