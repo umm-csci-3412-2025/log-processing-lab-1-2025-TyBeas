38839e9675d41c3ad42b7d005c6df50a92ecdbe1
TyBeas <beasl110@morris.umn.edu>
2025-02-04 14:38:59 -0600

    Differnet appraoch

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-cd "$1" || exit
-
-touch content.html
-touch failed_login_summary.html
-
-{
-    echo "<!-- ++++++++++++ START OF COUNTRY HEADER +++++++++++++++++++++++++++++++ -->"
-    cat ./country_dist.html
-    echo "<!-- ++++++++++++ END OF COUNTRY HEADER +++++++++++++++++++++++++++++++ -->"
-    echo "<!-- ++++++++++++ START OF HOURS HEADER +++++++++++++++++++++++++++++++ -->"
-    cat ./hours_dist.html
-    echo "<!-- ++++++++++++ END OF HOURS HEADER +++++++++++++++++++++++++++++++ -->"
-    echo "<!-- ++++++++++++ START OF USERNAME HEADER +++++++++++++++++++++++++++++++ -->"
-    cat ./username_dist.html
-    echo "<!-- ++++++++++++ END OF USERNAME HEADER +++++++++++++++++++++++++++++++ -->"
-} > content.html
-
-../bin/wrap_contents.sh ./content.html ../html_components/summary_plots ./failed_login_summary.html
+tmpfile=$(mktemp)
+cat "$1/country_dist.html" "$1/hours_dist.html" "$1/username_dist.html" > "$tmpfile"
+source bin/wrap_contents.sh "$tmpfile" html_components/summary_plots "$1/failed_login_summary.html"
