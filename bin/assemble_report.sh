61756b3c0efda6c9022aa2b46b74ff0c3e6f2577
TyBeas <beasl110@morris.umn.edu>
2025-02-04 14:34:55 -0600

    Trying something different

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -5,5 +5,16 @@ cd "$1" || exit
 touch content.html
 touch failed_login_summary.html
 
-cat ./country_dist.html ./hours_dist.html ./username_dist.html > content.html
+{
+    echo "<!-- ++++++++++++ START OF COUNTRY HEADER +++++++++++++++++++++++++++++++ -->"
+    cat ./country_dist.html
+    echo "<!-- ++++++++++++ END OF COUNTRY HEADER +++++++++++++++++++++++++++++++ -->"
+    echo "<!-- ++++++++++++ START OF HOURS HEADER +++++++++++++++++++++++++++++++ -->"
+    cat ./hours_dist.html
+    echo "<!-- ++++++++++++ END OF HOURS HEADER +++++++++++++++++++++++++++++++ -->"
+    echo "<!-- ++++++++++++ START OF USERNAME HEADER +++++++++++++++++++++++++++++++ -->"
+    cat ./username_dist.html
+    echo "<!-- ++++++++++++ END OF USERNAME HEADER +++++++++++++++++++++++++++++++ -->"
+} > content.html
+
 ../bin/wrap_contents.sh ./content.html ../html_components/summary_plots ./failed_login_summary.html
