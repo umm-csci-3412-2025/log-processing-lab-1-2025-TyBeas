e28a42d5051afa92b84774fd0723c343d039470d
TyBeas <beasl110@morris.umn.edu>
2025-02-04 19:59:05 -0600

    Changing back to 6/7 test pass

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-dir=$1
-
-cat "$dir"/country_dist.html "$dir"/hours_dist.html "$dir"/username_dist.html > "$dir"/summary_plot.txt
-./bin/wrap_contents.sh "$dir"/summary_plot.txt html_components/summary_plots "$dir"/failed_login_summary.html
-
-rm -rf "$dir"/summary_plot.txt
+cd "$1" || exit
+touch content.html
+touch failed_login_summary.html
+cat ./country_dist.html ./hours_dist.html ./username_dist.html > content.html
+../bin/wrap_contents.sh ./content.html ../html_components/summary_plots ./failed_login_summary.html
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir /tmp/SCRATCH
+here=$(pwd)
+mkdir data
 
-temp_dir=/tmp/SCRATCH
-
-for dir in "$@"
+for name in cscirepo_secure.tgz discovery_secure.tgz ganesha_secure.tgz mylar_secure.tgz velcro_secure.tgz zeus_secure.tgz
 do
-	computer=$(basename "$dir" .tgz)
-	mkdir -p "$temp_dir"/"$SCRATCH"
-	tar zxf "$dir" --directory "$temp_dir"/"$SCRATCH"
-	./bin/process_client_logs.sh "$temp_dir"/"$SCRATCH"
+        base=$(basename "$name" _secure.tgz)
+        mkdir ./data/"$base"
+        tar -xzf ./log_files/"$name" -C ./data/"$base"
+        ./bin/process_client_logs.sh ./data/"$base"
 done
 
-./bin/create_username_dist.sh "$temp_dir"
-./bin/create_hours_dist.sh "$temp_dir"
-./bin/create_country_dist.sh "$temp_dir"
-./bin/assemble_report.sh "$temp_dir"
-
-mv "$temp_dir"/failed_login_summary.html "$(pwd)"
-
-rm -rf "$temp_dir"
+./bin/create_username_dist.sh data
+./bin/create_hours_dist.sh data
+./bin/create_country_dist.sh data
+./bin/assemble_report.sh data
+mv ./data/failed_login_summary.html "$here"
+rm -rf ./data
