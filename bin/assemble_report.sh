ddc9d5b533bcc0ff6d58d7291e6bf8e93b2ffa7f
TyBeas <beasl110@morris.umn.edu>
2025-02-04 20:13:08 -0600

    fixing small error

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -4,4 +4,4 @@ cd "$1" || exit
 touch content.html
 touch failed_login_summary.html
 cat ./country_dist.html ./hours_dist.html ./username_dist.html > content.html
-../bin/wrap_contents.sh ./content.html ../html_components/summary_plots ./failed_login_summary.html
+"./bin/wrap_contents.sh ./content.html" "./html_components/summary_plots ./failed_login_summary.html"
